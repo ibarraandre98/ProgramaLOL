1471c5245fb22f0260e4f919956da53485de0cd1
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-11-21 20:46:19 -0600

    Funcionamiento

---
+++ ProgramaLOL/Principal.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ProgramaLOL
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(367, 384);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "btnCalcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.FormattingEnabled = true;
+            this.lbInfo.Location = new System.Drawing.Point(322, 171);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(120, 95);
+            this.lbInfo.TabIndex = 1;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lbInfo;
+    }
+}
+
---
+++ ProgramaLOL/Principal.cs
@@ -0,0 +1,48 @@
+﻿using SbsSW.SwiPlCs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaLOL
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Environment.SetEnvironmentVariable("Path", @"C:\\Program Files (x86)\\pl\\bin");
+                String[] p = { "-q", "-f", @"ejemplo.pl" };
+                PlEngine.Initialize(p);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lbInfo.Items.Clear();
+            PlQuery cargar = new PlQuery("cargar");
+            cargar.NextSolution();
+                PlQuery consulta = new PlQuery("amigos(X,Y)");
+                foreach (PlQueryVariables z in consulta.SolutionVariables)
+                    lbInfo.Items.Add(z["X"].ToString());
+
+
+                PlEngine.PlCleanup();
+            }
+    }
+}
